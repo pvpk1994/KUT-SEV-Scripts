0f33634236b73b81f719feb1a4c24ccc62556e82
Dr. Pavan Kumar Paluri <pvpk1994@gmail.com>
2023-01-02 21:14:08 -0600

    Launch QEMU script for Milan system with custom OVMF and QEMU path.

---
+++ run_qemu.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+qemu=/home/amd/Downloads/AMDSEV/snp-release-2022-08-22/usr/local/bin/qemu-system-x86_64
+ovmf=/home/amd/Downloads/AMDSEV/usr/local/share/qemu/OVMF_CODE.fd
+
+sudo $qemu --no-reboot \
+    -nodefaults -device pc-testdev -device isa-debug-exit,iobase=0xf4,iosize=0x4 \
+    -vnc none -serial stdio -device pci-testdev -machine accel=kvm \
+    -drive file=$ovmf,format=raw,if=pflash,readonly=on \
+    -drive file.dir=efi-tests/amd_sev/,file.driver=vvfat,file.rw=on,format=raw,if=virtio \
+    -net none -nographic -m 256 -smp 1 \
+    -machine q35,confidential-guest-support=sev0 -cpu EPYC-v4 \
+    -object sev-snp-guest,id=sev0,cbitpos=51,reduced-phys-bits=1 \
+    -debugcon file:/tmp/debug.log 2>/tmp/qemu.err
+
+
