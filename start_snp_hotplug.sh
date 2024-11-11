188e6c89581befccb97a67d0c0817b0a32a6d73c
Pavan Kumar Paluri <papaluri@amd.com>
2024-11-11 11:21:36 -0600

    Automate hotplugging of SEV-SNP guest memory
    Support for hotplugging more than 1 memory region has been introduced.
    
    Signed-off-by: Pavan Kumar Paluri <papaluri@amd.com>

--- start_snp_hotplug.sh
+++ start_snp_hotplug.sh
@@ -11,11 +11,17 @@
 
 # Run this script as follows: ./start_snp_hotplug.sh snp 512M
 GUEST_VM="$1"
-VM_MEM_SIZE="$2"
+NUM_MEM_REG="$2"
+VM_MEM_SIZE="$3"
+MAX_MEM_HP=10240
 
 offer_help() {
 	echo "Usage: $0 [options]"
-	echo "example: ./start_snp_hotplug.sh VM_NAME MEM"
+	echo
+	echo "example: ./start_snp_hotplug.sh arg_1 arg_2 arg_3"
+	echo "arg_1: Needs to have the same name as the QEMU's monitor name"
+	echo "arg_2: Number of memory regions to be hotplugged"
+	echo "arg_3: Memory size of each memory region (only in Megs)"
 	echo
 	echo "Options:"
 	echo " -h, --help offers help and example usage"
@@ -33,13 +39,24 @@ while [ -n "$1" ]; do
 	shift
 done
 
+if [ $((NUM_MEM_REG * VM_MEM_SIZE)) -gt $MAX_MEM_HP ]; then
+	echo "Number of memory regions to be hotplugged exceed the maximum memory allocated for hotplugging..."
+	offer_help
+fi
+
 if [ -z "$GUEST_VM" ]; then
 	echo "Please provide the name of the SNP guest VM that is currently running..."
 	offer_help
 fi
 
+if [ -z "$NUM_MEM_REG" ]; then
+	echo "Please provide the number of memory regions to be hotplugged..."
+	offer_help
+fi
+
 if [ -z "$VM_MEM_SIZE" ]; then
 	echo "Please provide the size of the memory to be hotplugged..."
+	echo "NOTE: All memory regions will have the same amount of memory that will be hotplugged"
 	offer_help
 fi
 
@@ -55,8 +72,12 @@ issue_command() {
 
 # Initiate memory hotplugging...
 add_mem_block() {
-	issue_command "object_add memory-backend-memfd,id=mem1,size=$VM_MEM_SIZE"
-	issue_command "device_add pc-dimm,id=dimm1,memdev=mem1"
+	for reg in $(seq 1 $NUM_MEM_REG)
+	do
+		issue_command "object_add memory-backend-memfd,id=mem$reg,size=${VM_MEM_SIZE}M"
+		issue_command "device_add pc-dimm,id=dimm$reg,memdev=mem$reg"
+		sleep 1
+	done
 }
 
 # verify the addition of hotplugged memory via QEMU mtr
@@ -68,5 +89,5 @@ verify_hotplug() {
 }
 
 add_mem_block
-echo "Memory hotplugging performed. Added $VM_MEM_SIZE Megs to $GUEST_VM guest"
+echo "Memory hotplugging performed for $NUM_MEM_REG memory blocks"
 verify_hotplug
